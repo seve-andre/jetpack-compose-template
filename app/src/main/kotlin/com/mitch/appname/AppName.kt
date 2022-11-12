85825af6e58a238842f8ae64c6422fee483f09b2
Andrea Severi <andrea.severi12@studio.unibo.it>
2022-11-12 15:18:34 +0100

    change timber config

--- app/src/main/kotlin/com/mitch/appname/AppName.kt
+++ app/src/main/kotlin/com/mitch/appname/AppName.kt
@@ -9,6 +9,8 @@ class AppName : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
