a3606911518e501bd0e0102b41db176398e7b099
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-01-28 18:32:42 +0100

    add monitoring to network state

--- app/src/main/kotlin/com/mitch/appname/MainActivity.kt
+++ app/src/main/kotlin/com/mitch/appname/MainActivity.kt
@@ -9,19 +9,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mitch.appname.ui.NavGraphs
 import com.mitch.appname.ui.theme.AppTheme
 import com.mitch.appname.ui.util.rememberAppState
+import com.mitch.appname.util.network.NetworkMonitor
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
+    @Inject
+    lateinit var networkMonitor: NetworkMonitor
+
+    @OptIn(
+        ExperimentalMaterial3Api::class,
+        ExperimentalLifecycleComposeApi::class
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,7 +43,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val appState = rememberAppState()
+                    val appState = rememberAppState(networkMonitor)
+                    val isOffline by appState.isOffline.collectAsStateWithLifecycle()
+
+                    LaunchedEffect(isOffline) {
+                        if (isOffline) {
+                            appState.snackbarHostState.showSnackbar(
+                                message = "not connected!",
+                                duration = SnackbarDuration.Indefinite,
+                            )
+                        }
+                    }
 
                     Scaffold(
                         modifier = Modifier.fillMaxSize(),
