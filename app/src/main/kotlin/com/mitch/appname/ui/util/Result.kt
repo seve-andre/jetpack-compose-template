3f6bab561e5d90226dee22ab1debf7b9c75bc563
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-01-28 18:30:13 +0100

    replace resource with result

---
+++ app/src/main/kotlin/com/mitch/appname/ui/util/Result.kt
@@ -0,0 +1,27 @@
+package com.mitch.appname.ui.util
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
+/**
+ * Result
+ *
+ * @param T
+ * @constructor Create empty Result
+ */
+sealed interface Result<out T> {
+    data class Success<T>(val data: T) : Result<T>
+    data class Error(val exception: Throwable? = null) : Result<Nothing>
+    object Loading : Result<Nothing>
+}
+
+fun <T> Flow<T>.asResult(): Flow<Result<T>> {
+    return this
+        .map<T, Result<T>> {
+            Result.Success(it)
+        }
+        .onStart { emit(Result.Loading) }
+        .catch { emit(Result.Error(it)) }
+}
