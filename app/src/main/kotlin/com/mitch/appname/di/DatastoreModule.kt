a69c0d1a5cbc80d9ccaefa7bb81033bdb332e966
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-01-29 20:16:18 +0100

    add datastore module provider

---
+++ app/src/main/kotlin/com/mitch/appname/di/DatastoreModule.kt
@@ -0,0 +1,34 @@
+package com.mitch.appname.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import com.mitch.appname.ProtoUserPreferences
+import com.mitch.appname.data.local.datastore.user.preferences.ProtoUserPreferencesSerializer
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatastoreModule {
+
+    @Provides
+    @Singleton
+    fun providesUserPreferencesDatastore(
+        @ApplicationContext context: Context,
+        protoUserPreferencesSerializer: ProtoUserPreferencesSerializer,
+    ): DataStore<ProtoUserPreferences> =
+        DataStoreFactory.create(
+            serializer = protoUserPreferencesSerializer,
+            scope = CoroutineScope(SupervisorJob())
+        ) {
+            context.dataStoreFile("user_preferences.pb")
+        }
+}
