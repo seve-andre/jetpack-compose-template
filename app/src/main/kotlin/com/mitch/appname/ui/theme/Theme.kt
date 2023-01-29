7fa5b831ad1ae6af7d770902d0c130886191e9ec
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-01-29 20:14:21 +0100

    add status bar color

--- app/src/main/kotlin/com/mitch/appname/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/mitch/appname/ui/theme/Theme.kt
@@ -1,10 +1,15 @@
 package com.mitch.appname.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme()
 private val LightColorScheme = lightColorScheme()
@@ -18,6 +23,18 @@ fun AppTheme(
         DarkColorScheme
     } else {
         LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        val currentWindow = (view.context as? Activity)?.window
+            ?: throw Exception("Not in an activity - unable to get Window reference")
+
+        SideEffect {
+            currentWindow.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars =
+                !darkTheme
+        }
     }
 
     MaterialTheme(
