37033f1ed02f2483f9cdb9f0eb69d3b737e98182
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-01-29 20:17:00 +0100

    add serializer for proto user preferences

---
+++ app/src/main/kotlin/com/mitch/appname/data/local/datastore/user/preferences/ProtoUserPreferencesSerializer.kt
@@ -0,0 +1,21 @@
+package com.mitch.appname.data.local.datastore.user.preferences
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import com.mitch.appname.ProtoUserPreferences
+import java.io.InputStream
+import java.io.OutputStream
+
+object ProtoUserPreferencesSerializer : Serializer<ProtoUserPreferences> {
+    override val defaultValue: ProtoUserPreferences = ProtoUserPreferences.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): ProtoUserPreferences =
+        try {
+            ProtoUserPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+
+    override suspend fun writeTo(t: ProtoUserPreferences, output: OutputStream) = t.writeTo(output)
+}
