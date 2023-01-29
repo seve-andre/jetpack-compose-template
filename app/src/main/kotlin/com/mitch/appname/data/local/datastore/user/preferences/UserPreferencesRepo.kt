a5621b3323400fff8cbe824508195d76a68cfa8c
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-01-29 20:19:24 +0100

    add local user preferences repo and implementation

---
+++ app/src/main/kotlin/com/mitch/appname/data/local/datastore/user/preferences/UserPreferencesRepo.kt
@@ -0,0 +1,12 @@
+package com.mitch.appname.data.local.datastore.user.preferences
+
+import com.mitch.appname.util.AppLanguage
+import com.mitch.appname.util.AppTheme
+import kotlinx.coroutines.flow.Flow
+
+interface UserPreferencesRepo {
+    suspend fun setLanguage(language: AppLanguage)
+    suspend fun setTheme(theme: AppTheme)
+    fun getLanguage(): Flow<AppLanguage>
+    fun getTheme(): Flow<AppTheme>
+}
---
+++ app/src/main/kotlin/com/mitch/appname/data/local/datastore/user/preferences/UserPreferencesRepoImpl.kt
@@ -0,0 +1,26 @@
+package com.mitch.appname.data.local.datastore.user.preferences
+
+import com.mitch.appname.data.mapper.toLocal
+import com.mitch.appname.data.mapper.toProto
+import com.mitch.appname.util.AppLanguage
+import com.mitch.appname.util.AppTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class UserPreferencesRepoImpl @Inject constructor(
+    private val userPreferencesDataSource: UserPreferencesDataSource
+) : UserPreferencesRepo {
+
+    override suspend fun setLanguage(language: AppLanguage) =
+        userPreferencesDataSource.setProtoLanguage(language.toProto())
+
+    override suspend fun setTheme(theme: AppTheme) =
+        userPreferencesDataSource.setProtoTheme(theme.toProto())
+
+    override fun getLanguage(): Flow<AppLanguage> =
+        userPreferencesDataSource.getProtoLanguage().map { it.toLocal() }
+
+    override fun getTheme(): Flow<AppTheme> =
+        userPreferencesDataSource.getProtoTheme().map { it.toLocal() }
+}
