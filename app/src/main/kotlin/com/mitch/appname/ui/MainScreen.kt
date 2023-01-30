30c854bb9c12526722bed1330ed8e6ceac494211
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-01-30 23:19:10 +0100

    remove useless imports

--- app/src/main/kotlin/com/mitch/appname/ui/MainScreen.kt
+++ app/src/main/kotlin/com/mitch/appname/ui/MainScreen.kt
@@ -1,28 +1,8 @@
 package com.mitch.appname.ui
 
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.unit.dp
-import androidx.core.os.LocaleListCompat
-import com.mitch.appname.util.AppLanguage
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
-import java.util.Locale
 
 @RootNavGraph(start = true)
 @Destination
