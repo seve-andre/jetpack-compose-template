5caad4d2c6a8bd7833f15e1c1a0b678c31964a3e
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-04-17 11:08:41 +0200

    add extensions for layout

---
+++ app/src/main/kotlin/com/mitch/appname/ui/util/extensions/m3/Layout.kt
@@ -0,0 +1,48 @@
+package com.mitch.appname.ui.util.extensions.m3
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.mitch.appname.ui.theme.custom.padding
+
+// padding
+
+// https://m3.material.io/foundations/layout/applying-layout/compact#4b2b6814-c64a-4bc0-a07d-6652a91737e6
+val WindowWidthSizeClass.compactPadding: PaddingValues
+    get() = PaddingValues(padding.medium)
+
+// https://m3.material.io/foundations/layout/applying-layout/medium#4899a0c6-bc71-4e86-8095-39e5d517db6a
+val WindowWidthSizeClass.mediumPadding: PaddingValues
+    get() = PaddingValues(24.dp)
+
+// https://m3.material.io/foundations/layout/applying-layout/expanded#3f62eeac-33c3-4639-8d06-0cb091d8e7f5
+val WindowWidthSizeClass.expandedPadding: PaddingValues
+    get() = PaddingValues(24.dp)
+
+// width - https://developer.android.com/guide/topics/large-screens/support-different-screen-sizes
+val WindowWidthSizeClass.compactMaxWidth: Dp
+    get() = 600.dp
+
+val WindowWidthSizeClass.mediumMinWidth: Dp
+    get() = compactMaxWidth
+
+val WindowWidthSizeClass.mediumMaxWidth: Dp
+    get() = 840.dp
+
+val WindowWidthSizeClass.expandedMinWidth: Dp
+    get() = mediumMaxWidth
+
+// height - https://developer.android.com/guide/topics/large-screens/support-different-screen-sizes
+val WindowHeightSizeClass.compactMaxHeight: Dp
+    get() = 480.dp
+
+val WindowHeightSizeClass.mediumMinHeight: Dp
+    get() = compactMaxHeight
+
+val WindowHeightSizeClass.mediumMaxHeight: Dp
+    get() = 900.dp
+
+val WindowHeightSizeClass.expandedMinHeight: Dp
+    get() = mediumMaxHeight
