cabb46dd5b96ac39f09d76b81eefe3fecd760705
Andrea Severi <andrea.severi.dev@gmail.com>
2023-08-24 19:22:06 +0200

    upgrade to gradle v8.1

--- app/src/main/kotlin/com/mitch/appname/AppName.kt
+++ app/src/main/kotlin/com/mitch/appname/AppName.kt
@@ -2,7 +2,6 @@ package com.mitch.appname
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
 class AppName : Application() {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.application) apply false
