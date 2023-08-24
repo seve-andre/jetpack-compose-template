f2f158787c29550466c6e7c77bf72eaa43729aa4
Andrea Severi <andrea.severi.dev@gmail.com>
2023-08-24 09:29:35 +0200

    remove protobuf imports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,3 @@
-import com.google.protobuf.gradle.builtins
-import com.google.protobuf.gradle.generateProtoTasks
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
-
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application)
