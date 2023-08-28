56728b873f41ce2011bc40a4bb417d59ae1425d1
Andrea Severi <andrea.severi.dev@gmail.com>
2023-08-28 22:05:29 +0200

    fix package name

--- app/src/androidTest/kotlin/com/mitch/appname/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/com/mitch/appname/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mitch.gradlektstest", appContext.packageName)
+        assertEquals("com.mitch.appname", appContext.packageName)
     }
 }
