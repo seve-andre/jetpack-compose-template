f1edfe35bc2a1162ac6b3356da0266fd49d9623a
Andrea Severi <andrea.severi.dev@gmail.com>
2023-08-31 13:08:54 +0200

    pass locale to app language instead of language tag

--- app/src/main/kotlin/com/mitch/appname/util/AppLanguage.kt
+++ app/src/main/kotlin/com/mitch/appname/util/AppLanguage.kt
@@ -2,11 +2,7 @@ package com.mitch.appname.util
 
 import java.util.Locale
 
-enum class AppLanguage(private val languageTag: String) {
-    ENGLISH("en"),
-    ITALIAN("it");
-
-    fun toLocale(): Locale {
-        return Locale.forLanguageTag(this.languageTag)
-    }
+enum class AppLanguage(val locale: Locale) {
+    ENGLISH(Locale.ENGLISH),
+    ITALIAN(Locale.ITALIAN)
 }
