2cea1712283691fe066878bc56cb96e308f23f02
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-04 23:46:40 +0200

    convert to data object

--- app/src/main/kotlin/com/mitch/appname/ui/util/Result.kt
+++ app/src/main/kotlin/com/mitch/appname/ui/util/Result.kt
@@ -1,5 +1,8 @@
 package com.mitch.appname.ui.util
 
+import com.mitch.appname.ui.util.Result.Error
+import com.mitch.appname.ui.util.Result.Loading
+import com.mitch.appname.ui.util.Result.Success
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
@@ -18,7 +21,7 @@ import kotlinx.coroutines.flow.onStart
 sealed interface Result<out T> {
     data class Success<T>(val data: T) : Result<T>
     data class Error(val exception: Throwable? = null) : Result<Nothing>
-    object Loading : Result<Nothing>
+    data object Loading : Result<Nothing>
 }
 
 /**
@@ -30,8 +33,8 @@ sealed interface Result<out T> {
 fun <T> Flow<T>.asResult(): Flow<Result<T>> {
     return this
         .map<T, Result<T>> {
-            Result.Success(it)
+            Success(it)
         }
-        .onStart { emit(Result.Loading) }
-        .catch { emit(Result.Error(it)) }
+        .onStart { emit(Loading) }
+        .catch { emit(Error(it)) }
 }
