cc4d7688f124f853691e7ab2a16e12984c62636f
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-05 23:51:38 +0200

    create tests for change settings option

--- app/src/androidTest/kotlin/com/mitch/appname/ui/screens/home/components/LanguagePickerDialogTest.kt
+++ app/src/androidTest/kotlin/com/mitch/appname/ui/screens/home/components/LanguagePickerDialogTest.kt
@@ -2,10 +2,12 @@ package com.mitch.appname.ui.screens.home.components
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotSelected
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.mitch.appname.util.AppLanguage
 import org.junit.Rule
 import org.junit.Test
@@ -16,7 +18,7 @@ class LanguagePickerDialogTest {
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
-    fun test_showsAllLanguageOptionsAreDisplayed() {
+    fun showsAllLanguageOptionsAreDisplayed() {
         composeTestRule.setContent {
             LanguagePickerDialog(
                 selectedLanguage = AppLanguage.default(),
@@ -40,6 +42,34 @@ class LanguagePickerDialogTest {
         // default language is selected
         composeTestRule
             .onNodeWithText(AppLanguage.default().locale.displayLanguage)
+            .assertIsSelected()
+    }
+
+    @Test
+    fun whenNewSelected_displaysCorrectOption() {
+        composeTestRule.setContent {
+            LanguagePickerDialog(
+                selectedLanguage = AppLanguage.default(),
+                onDismiss = { },
+                onConfirm = { }
+            )
+        }
+
+        val newOption = AppLanguage.values().filter { it != AppLanguage.default() }[0]
+
+        // click on new one
+        composeTestRule
+            .onNodeWithText(newOption.locale.displayLanguage)
+            .performClick()
+
+        // old one is not selected
+        composeTestRule
+            .onNodeWithText(AppLanguage.default().locale.displayLanguage)
+            .assertIsNotSelected()
+
+        // but new one is
+        composeTestRule
+            .onNodeWithText(newOption.locale.displayLanguage)
             .assertIsSelected()
     }
 }
--- app/src/androidTest/kotlin/com/mitch/appname/ui/screens/home/components/ThemePickerDialogTest.kt
+++ app/src/androidTest/kotlin/com/mitch/appname/ui/screens/home/components/ThemePickerDialogTest.kt
@@ -3,9 +3,11 @@ package com.mitch.appname.ui.screens.home.components
 import androidx.activity.ComponentActivity
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotSelected
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.mitch.appname.util.AppTheme
 import org.junit.Rule
 import org.junit.Test
@@ -18,7 +20,7 @@ class ThemePickerDialogTest {
     private fun getString(@StringRes id: Int) = composeTestRule.activity.resources.getString(id)
 
     @Test
-    fun test_showsAllThemeOptionsAreDisplayed() {
+    fun showsAllThemeOptionsAreDisplayed() {
         composeTestRule.setContent {
             ThemePickerDialog(
                 selectedTheme = AppTheme.default(),
@@ -35,6 +37,34 @@ class ThemePickerDialogTest {
         // default theme is selected
         composeTestRule
             .onNodeWithText(getString(AppTheme.default().translationId))
+            .assertIsSelected()
+    }
+
+    @Test
+    fun whenNewSelected_displaysCorrectOption() {
+        composeTestRule.setContent {
+            ThemePickerDialog(
+                selectedTheme = AppTheme.default(),
+                onDismiss = { },
+                onConfirm = { }
+            )
+        }
+
+        val newOption = AppTheme.values().filter { it != AppTheme.default() }[0]
+
+        // click on new one
+        composeTestRule
+            .onNodeWithText(getString(newOption.translationId))
+            .performClick()
+
+        // old one is not selected
+        composeTestRule
+            .onNodeWithText(getString(AppTheme.default().translationId))
+            .assertIsNotSelected()
+
+        // but new one is
+        composeTestRule
+            .onNodeWithText(getString(newOption.translationId))
             .assertIsSelected()
     }
 }
