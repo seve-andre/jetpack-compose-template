405954bd0ae342da66ca2052179b3d7d8e1d9185
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-19 23:11:52 +0200

    create toast controller class to display it one at a time

--- app/src/main/kotlin/com/mitch/appname/util/ToastController.kt
+++ app/src/main/kotlin/com/mitch/appname/util/ToastController.kt
@@ -1,1 +1,32 @@
 package com.mitch.appname.util
+
+import android.content.Context
+import android.widget.Toast
+
+object ToastController {
+
+    private var showingToast: Toast? = null
+    fun show(
+        context: Context,
+        message: String,
+        duration: ToastDuration = ToastDuration.SHORT
+    ) {
+        if (showingToast != null) {
+            showingToast?.cancel()
+        }
+        showingToast = Toast.makeText(
+            context,
+            message,
+            when (duration) {
+                ToastDuration.SHORT -> Toast.LENGTH_SHORT
+                ToastDuration.LONG -> Toast.LENGTH_LONG
+            }
+        )
+        showingToast?.show()
+    }
+}
+
+enum class ToastDuration {
+    SHORT,
+    LONG
+}
