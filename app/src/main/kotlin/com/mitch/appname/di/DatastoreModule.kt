2e346e8367025f1d12b843a06bb6eb8bf0236d11
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-28 10:23:52 +0200

    correct imports order

--- app/src/main/kotlin/com/mitch/appname/di/DatastoreModule.kt
+++ app/src/main/kotlin/com/mitch/appname/di/DatastoreModule.kt
@@ -11,9 +11,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
