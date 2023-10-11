9478de50166f52fd93691817a50629ed350f70e0
Andrea Severi <andrea.severi.dev@gmail.com>
2023-10-11 23:52:59 +0200

    remove kapt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.kapt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.detekt) apply false
