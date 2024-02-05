92c1176a02c160c6d7592a4a049742a419aeabe4
Andrea Severi <andrea.severi.dev@gmail.com>
2024-02-05 20:16:23 +0100

    add completely customizable snackbar

---
+++ app/src/main/kotlin/com/mitch/appname/ui/designsystem/AppIcons.kt
@@ -0,0 +1,13 @@
+package com.mitch.appname.ui.designsystem
+
+import compose.icons.EvaIcons
+import compose.icons.evaicons.Fill
+import compose.icons.evaicons.fill.AlertTriangle
+import compose.icons.evaicons.fill.CheckmarkCircle2
+import compose.icons.evaicons.fill.CloseCircle
+
+object AppIcons {
+    val Success = EvaIcons.Fill.CheckmarkCircle2
+    val Warning = EvaIcons.Fill.AlertTriangle
+    val Error = EvaIcons.Fill.CloseCircle
+}
