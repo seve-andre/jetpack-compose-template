2e6289581be56f15a94cf8cb1641a3ae81d8a43f
Andrea Severi <andrea.severi.dev@gmail.com>
2024-05-13 18:40:54 +0200

    remove example screen

--- app/src/main/kotlin/com/mitch/template/ui/navigation/TemplateNavigation.kt
+++ app/src/main/kotlin/com/mitch/template/ui/navigation/TemplateNavigation.kt
@@ -7,9 +7,6 @@ object TemplateNavigation {
     sealed interface Screen {
         @Serializable
         data object Home : Screen
-
-        @Serializable
-        data class Profile(val id: Int) : Screen
     }
 
     sealed interface Graph
