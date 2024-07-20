73e8a7ea38afb80af61298d9838a4e1177750e47
Andrea Severi <andrea.severi.dev@gmail.com>
2024-07-20 14:07:28 +0200

    indent string better

--- lint/src/test/kotlin/com/mitch/template/lint/util/Stubs.kt
+++ lint/src/test/kotlin/com/mitch/template/lint/util/Stubs.kt
@@ -5,7 +5,7 @@ import com.android.tools.lint.checks.infrastructure.TestFile
 
 val ComposableStub: TestFile = kotlin(
     """
-package androidx.compose.runtime
-annotation class Composable
-"""
+    package androidx.compose.runtime
+    annotation class Composable
+    """
 ).indented()
