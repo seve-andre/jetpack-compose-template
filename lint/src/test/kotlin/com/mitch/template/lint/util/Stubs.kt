f451d6f0e95b8623a697546edc6c461cb7a40beb
Andrea Severi <andrea.severi.dev@gmail.com>
2024-07-20 13:56:26 +0200

    add stub for composable tests
    Former-commit-id: 0e35d50bbb49f440c9810ae13bc7f379cbd1150e

---
+++ lint/src/test/kotlin/com/mitch/template/lint/util/Stubs.kt
@@ -0,0 +1,11 @@
+package com.mitch.template.lint.util
+
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest.kotlin
+import com.android.tools.lint.checks.infrastructure.TestFile
+
+val ComposableStub: TestFile = kotlin(
+    """
+package androidx.compose.runtime
+annotation class Composable
+"""
+).indented()
