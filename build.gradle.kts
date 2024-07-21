cad5a16ee490418ce7c4aba928a79d63b4e0f6fd
Andrea Severi <andrea.severi.dev@gmail.com>
2024-07-21 18:07:57 +0200

    apply linting to lint module itself

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ plugins {
     alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.secrets) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.android.lint) apply false
 }
