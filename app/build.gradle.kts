5dcda5b198bf29976545d3f097cce5e5979c208d
Andrea Severi <andrea.severi.dev@gmail.com>
2024-09-15 21:18:09 +0200

    upgrade to sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,11 +47,11 @@ keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 android {
     namespace = packageName
 
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = packageName
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.0.1" // X.Y.Z; X = Major, Y = minor, Z = Patch level
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
