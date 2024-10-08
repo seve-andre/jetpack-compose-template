388a65e77187599aa3af8b8590e500998e9c8c8a
Andrea Severi <andrea.severi.dev@gmail.com>
2024-10-08 22:54:18 +0200

    replace retrofit with ktor and provide http client

--- app/src/main/kotlin/com/mitch/template/di/DependenciesProvider.kt
+++ app/src/main/kotlin/com/mitch/template/di/DependenciesProvider.kt
@@ -5,6 +5,7 @@ import com.mitch.template.data.TemplateDatabase
 import com.mitch.template.data.settings.UserSettingsRepository
 import com.mitch.template.domain.models.UserPreferences
 import com.mitch.template.util.network.NetworkMonitor
+import io.ktor.client.HttpClient
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.serialization.json.Json
@@ -18,4 +19,5 @@ interface DependenciesProvider {
     val jsonSerializer: Json
     val database: TemplateDatabase
     val preferencesDataStore: DataStore<UserPreferences>
+    val httpClient: HttpClient
 }
