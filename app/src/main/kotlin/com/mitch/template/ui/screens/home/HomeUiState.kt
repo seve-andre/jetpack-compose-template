b42742dd238073c06830bce7a44cfda0eb5238c2
Andrea Severi <andrea.severi.dev@gmail.com>
2025-12-09 17:43:16 +0100

    improve home screen and its dialogs compose stability

--- app/src/main/kotlin/com/mitch/template/ui/screens/home/HomeUiState.kt
+++ app/src/main/kotlin/com/mitch/template/ui/screens/home/HomeUiState.kt
@@ -1,8 +1,10 @@
 package com.mitch.template.ui.screens.home
 
+import androidx.compose.runtime.Stable
 import com.mitch.template.domain.models.TemplateLanguagePreference
 import com.mitch.template.domain.models.TemplateThemePreference
 
+@Stable
 sealed interface HomeUiState {
     data object Loading : HomeUiState
 
--- app/src/main/kotlin/com/mitch/template/ui/screens/home/components/ThemePickerDialog.kt
+++ app/src/main/kotlin/com/mitch/template/ui/screens/home/components/ThemePickerDialog.kt
@@ -17,8 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,14 +35,17 @@ import com.mitch.template.ui.designsystem.TemplateDesignSystem
 import com.mitch.template.ui.designsystem.TemplateIcons
 import com.mitch.template.ui.designsystem.TemplateTheme
 import com.mitch.template.ui.designsystem.theme.custom.padding
+import kotlinx.collections.immutable.PersistentSet
+import kotlinx.collections.immutable.toPersistentSet
 
 @Composable
 fun ThemePickerDialog(
-    selectedTheme: TemplateThemePreference,
     onDismiss: () -> Unit,
-    onConfirm: (TemplateThemePreference) -> Unit
+    onConfirm: (TemplateThemePreference) -> Unit,
+    selectedTheme: TemplateThemePreference,
+    themeOptions: PersistentSet<TemplateThemePreference>
 ) {
-    var tempTheme by remember { mutableStateOf(selectedTheme) }
+    var tempTheme by rememberSaveable { mutableStateOf(selectedTheme) }
 
     AlertDialog(
         onDismissRequest = onDismiss,
@@ -56,40 +60,19 @@ fun ThemePickerDialog(
         },
         text = {
             Column(modifier = Modifier.selectableGroup()) {
-                for (themePreference in TemplateThemePreference.entries) {
+                for (themePreference in themeOptions) {
                     val isSelected = themePreference == tempTheme
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(56.dp)
-                            .clip(RoundedCornerShape(16.dp))
-                            .selectable(
-                                selected = isSelected,
-                                onClick = { tempTheme = themePreference },
-                                role = Role.RadioButton
-                            )
-                            .padding(horizontal = TemplateDesignSystem.padding.medium),
-                        horizontalArrangement = Arrangement.spacedBy(TemplateDesignSystem.padding.medium),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        RadioButton(
-                            selected = isSelected,
-                            onClick = null // null recommended for accessibility with screen readers
+
+                    key(themePreference.ordinal) {
+                        ThemeOptionRadioButton(
+                            isSelected = isSelected,
+                            onClick = {
+                                if (!isSelected) {
+                                    tempTheme = themePreference
+                                }
+                            },
+                            themePreference = themePreference
                         )
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(TemplateDesignSystem.padding.small),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Icon(
-                                imageVector = themePreference.icon(),
-                                contentDescription = null,
-                                modifier = Modifier.size(20.dp)
-                            )
-                            Text(
-                                text = themePreference.title(),
-                                style = TemplateDesignSystem.typography.bodyLarge
-                            )
-                        }
                     }
                 }
             }
@@ -111,6 +94,47 @@ fun ThemePickerDialog(
             }
         }
     )
+}
+
+@Composable
+private fun ThemeOptionRadioButton(
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    themePreference: TemplateThemePreference
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .selectable(
+                selected = isSelected,
+                onClick = onClick,
+                role = Role.RadioButton
+            )
+            .padding(horizontal = TemplateDesignSystem.padding.medium),
+        horizontalArrangement = Arrangement.spacedBy(TemplateDesignSystem.padding.medium),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(
+            selected = isSelected,
+            onClick = null // null recommended for accessibility with screen readers
+        )
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(TemplateDesignSystem.padding.small),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = themePreference.icon(),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = themePreference.title(),
+                style = TemplateDesignSystem.typography.bodyLarge
+            )
+        }
+    }
 }
 
 @Composable
@@ -137,9 +161,10 @@ private fun TemplateThemePreference.title(): String {
 private fun ThemePickerDialogPreview() {
     TemplateTheme {
         ThemePickerDialog(
-            selectedTheme = TemplateThemePreference.Dark,
             onDismiss = { },
-            onConfirm = { }
+            onConfirm = { },
+            selectedTheme = TemplateThemePreference.Dark,
+            themeOptions = TemplateThemePreference.entries.toPersistentSet()
         )
     }
 }
