df838f654879ff9ece7cbbacde666282ab9d8b62
Andrea Severi <andrea.severi.dev@gmail.com>
2025-12-10 12:26:38 +0100

    replace @PreviewLightDark with custom to show background and system ui in preview

--- app/src/main/kotlin/com/mitch/template/ui/screens/home/HomeScreen.kt
+++ app/src/main/kotlin/com/mitch/template/ui/screens/home/HomeScreen.kt
@@ -15,7 +15,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_TYPE_NORMAL
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewFontScale
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -95,8 +98,15 @@ private enum class ActiveDialog {
     None, Language, Theme
 }
 
-@PreviewLightDark
+@Preview(name = "Light", showBackground = true, showSystemUi = true)
+@Preview(
+    name = "Dark",
+    uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL,
+    showBackground = true,
+    showSystemUi = true
+)
 @PreviewScreenSizes
+@PreviewFontScale
 @Composable
 private fun HomeScreenContentPreview() {
     TemplateTheme {
