1b1cee0cfc733a3807cc13f5dc92abb22b4155ec
Andrea Severi <andrea.severi.dev@gmail.com>
2026-01-19 09:58:38 +0100

    improve loading screen accessibility

--- app/src/main/kotlin/com/mitch/template/ui/designsystem/components/loading/LoadingScreen.kt
+++ app/src/main/kotlin/com/mitch/template/ui/designsystem/components/loading/LoadingScreen.kt
@@ -7,21 +7,28 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-
-const val LoadingTag: String = "loading indicator"
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.LiveRegionMode
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.liveRegion
+import com.mitch.template.R
 
 @Composable
 fun LoadingScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    contentDescription: String = stringResource(R.string.loading_default_content_description)
 ) {
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .testTag(LoadingTag),
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        CircularProgressIndicator()
+        CircularProgressIndicator(
+            modifier = Modifier.clearAndSetSemantics {
+                this.contentDescription = contentDescription
+                liveRegion = LiveRegionMode.Polite
+            }
+        )
     }
 }
